248ed030d0e96f8944f02dc1d12327cfcfc71c07
Henrique Vaz <henrique.m.vaz@tecnico.ulisboa.pt>
2024-03-08 01:00:26 +0000

    Implementation of comparison, conditional and let form without functions

--- main.jl
+++ main.jl
@@ -6,9 +6,51 @@ function metajulia_repl()
     metajulia_repl()
 end
 
+global_scope = Dict{Symbol, Any}()
+
+function handleIf(expr)
+    i = 1
+    args = map(evaluate, expr.args[1:end])
+    while i < length(args)
+        condition = evaluate(args[1])
+        if condition
+            return args[2]
+        else
+            i += 2
+        end
+    end
+    return map(evaluate, args[end])
+end
+
+function handleComparison(expr)
+    i = 3
+    new_expr = Expr(:call, expr.args[2], expr.args[1], expr.args[3])
+    while i < length(expr.args)
+        new_expr = Expr(:&&, new_expr, Expr(:call, expr.args[i+1], expr.args[i], expr.args[i+2]))
+        i += 2
+    end
+    args = map(evaluate, new_expr.args[1:end])
+    return args[1] && args[2]
+end
+
+function handleLet(expr)
+    if isa(expr.args[1].args[1], Expr)
+        i = 1
+        while i <= length(expr.args[1].args)
+            global_scope[expr.args[1].args[i].args[1]] = expr.args[1].args[i].args[2]
+            i += 1
+        end 
+    else
+        global_scope[expr.args[1].args[1]] = expr.args[1].args[2]
+    end
+    return evaluate(expr.args[end])
+end
+
 function evaluate(expr)
     if isa(expr, Number) || isa(expr, String)
         return expr
+    elseif isa(expr, Symbol)
+        get(global_scope, expr, nothing)
     elseif isa(expr, Expr)
         if expr.head == :call
             symb = expr.args[1]
@@ -32,20 +74,15 @@ function evaluate(expr)
         elseif expr.head == :||
             args = map(evaluate, expr.args[1:end])
             return args[1] || args[2]
-        elseif expr.head == :if
-            i = 1
-            while i < length(expr.args)
-                condition = evaluate(expr.args[i])
-                if condition
-                    return expr.args[i+1]
-                else
-                    i += 2
-                end
-            end
-            return evaluate(expr.args[length(expr.args)])
+        elseif expr.head == :comparison
+            handleComparison(expr)
+        elseif expr.head == :if || expr.head == :elseif
+            handleIf(expr)
         elseif expr.head == :block
             args = map(evaluate, expr.args[1:end])
             return args[end]
+        elseif expr.head == :let
+            return handleLet(expr)
         end
     end
 end
